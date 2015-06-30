9454673723938a4498a51c7cc2971e3f8e784849
Nathan Lakes <nathan.lakes@gmail.com>
2015-06-30 10:48:53 +0930

    Initial Upload

---
+++ Assignment3_LHISGroup/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Assignment3_LHISGroup
+{
+
+    /**
+     *   The controller for the SQL Database model. 
+     *   All views interface with Database.cs and read
+     *   write via this class
+     */   
+    class Database
+    {
+
+        SqlConnection db;
+
+
+        public Database(string username, string password, string serverurl, 
+                        string trustConn, string dbb, int timeOut)
+        {
+
+            try
+            {
+                db = new SqlConnection("user id=" + username + ";" +
+                                    "password=" + password + ";" +
+                                    "server=" + serverurl + ";" +
+                                    "Trusted_Connection=" + trustConn + ";" +
+                                    "database=" + dbb + ";" +
+                                    "connection timeout=" + timeOut);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error connecting to the database.");
+            }
+            
+        }
+
+        
+        
+
+    }
+}
