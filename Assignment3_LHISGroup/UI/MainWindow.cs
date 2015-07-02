0feebd4a864badd8c6fb21ba352aa177a0107bf1
stodt003 <stodt003@mymail.unisa.edu.au>
2015-07-02 12:59:26 +0930

    Added access to staff report window

--- Assignment3_LHISGroup/UI/MainWindow.cs
+++ Assignment3_LHISGroup/UI/MainWindow.cs
@@ -183,7 +183,7 @@ namespace Assignment3_LHISGroup
 
         private void ReportsButton_Click(object sender, EventArgs e)
         {
-            UI.EventReportWindow win = new UI.EventReportWindow(this);
+            UI.ReportsMainWindow win = new UI.ReportsMainWindow(this);
             win.Visible = true;
         }
     }
--- Assignment3_LHISGroup/UI/StaffReportWindow.Designer.cs
+++ Assignment3_LHISGroup/UI/StaffReportWindow.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Assignment3_LHISGroup.Reports
+﻿namespace Assignment3_LHISGroup.UI
 {
-    partial class StaffReport
+    partial class StaffReportWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StaffTasksListView = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
-            // StaffReport
+            // StaffTasksListView
+            // 
+            this.StaffTasksListView.Location = new System.Drawing.Point(38, 87);
+            this.StaffTasksListView.Name = "StaffTasksListView";
+            this.StaffTasksListView.Size = new System.Drawing.Size(419, 255);
+            this.StaffTasksListView.TabIndex = 0;
+            this.StaffTasksListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // StaffReportWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 373);
-            this.Name = "StaffReport";
-            this.Text = "StaffReport";
-            this.Load += new System.EventHandler(this.StaffReport_Load);
+            this.ClientSize = new System.Drawing.Size(500, 448);
+            this.Controls.Add(this.StaffTasksListView);
+            this.Name = "StaffReportWindow";
+            this.Text = "Staff Report ";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView StaffTasksListView;
     }
 }
