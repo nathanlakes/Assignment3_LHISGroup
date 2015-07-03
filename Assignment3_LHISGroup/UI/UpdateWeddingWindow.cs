37e8063204858a34e4ced3b3e4b6d7e4fcd38902
Louise Lawrence <lawlm0004@mymail.unisa.edu.au>
2015-07-03 23:35:31 +0930

    More Window ComboBoxes/DropDownLists fixes

--- Assignment3_LHISGroup/UI/UpdateWeddingWindow.cs
+++ Assignment3_LHISGroup/UI/UpdateWeddingWindow.cs
@@ -14,12 +14,51 @@ namespace Assignment3_LHISGroup.UI
     {
         MainWindow mainWin;
         DbController db;
-        
+
+        List<Support_Classes.Staff> StaffList;
+        List<Support_Classes.Client> ClientList;
+
+
         public UpdateWeddingWindow(MainWindow w, DbController d)
         {
             InitializeComponent();
             db = d;
             mainWin = w;
+
+            ClientList = db.GetAllClients();
+            foreach (Support_Classes.Client client in ClientList)
+            {
+                ClientComboBox.Items.Add(client.Firstname + " " + client.Surname);
+                EngagedComboBox.Items.Add(client.Firstname + " " + client.Surname);
+            }
+
+            StaffList = db.GetAllStaff();
+            foreach (Support_Classes.Staff staff in StaffList)
+            {
+                if (staff.StatusToString().Equals("active"))
+                {
+                    StaffComboBox.Items.Add(staff.FirstName + " " + staff.Surname);
+                }
+            }
+        }
+
+        public void RefreshData()
+        {
+            ClientList = db.GetAllClients();
+            foreach (Support_Classes.Client client in ClientList)
+            {
+                ClientComboBox.Items.Add(client.Firstname + " " + client.Surname);
+                EngagedComboBox.Items.Add(client.Firstname + " " + client.Surname);
+            }
+
+            StaffList = db.GetAllStaff();
+            foreach (Support_Classes.Staff staff in StaffList)
+            {
+                if (staff.StatusToString().Equals("active"))
+                {
+                    StaffComboBox.Items.Add(staff.FirstName + " " + staff.Surname);
+                }
+            }
         }
 
         private void ReportsButton_Click(object sender, EventArgs e)
