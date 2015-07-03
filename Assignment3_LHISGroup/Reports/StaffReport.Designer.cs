01401cfdb4c2a35e5e532c79d92acba9d2e61a1a
stodt003 <stodt003@mymail.unisa.edu.au>
2015-07-03 23:37:38 +0930

    Staff Report Update

--- Assignment3_LHISGroup/Reports/StaffReport.Designer.cs
+++ Assignment3_LHISGroup/Reports/StaffReport.Designer.cs
@@ -28,20 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StaffListBox = new System.Windows.Forms.ListBox();
+            this.StaffListLabel = new System.Windows.Forms.Label();
+            this.TasksView = new System.Windows.Forms.DataGridView();
+            this.Tasks = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TaskCompleteBy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.TasksView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // StaffListBox
+            // 
+            this.StaffListBox.FormattingEnabled = true;
+            this.StaffListBox.Location = new System.Drawing.Point(12, 44);
+            this.StaffListBox.Name = "StaffListBox";
+            this.StaffListBox.Size = new System.Drawing.Size(175, 134);
+            this.StaffListBox.TabIndex = 0;
+            this.StaffListBox.SelectedIndexChanged += new System.EventHandler(this.StaffListBox_SelectedIndexChanged);
+            // 
+            // StaffListLabel
+            // 
+            this.StaffListLabel.AutoSize = true;
+            this.StaffListLabel.Location = new System.Drawing.Point(13, 25);
+            this.StaffListLabel.Name = "StaffListLabel";
+            this.StaffListLabel.Size = new System.Drawing.Size(81, 13);
+            this.StaffListLabel.TabIndex = 1;
+            this.StaffListLabel.Text = "Active Staff List";
+            // 
+            // TasksView
+            // 
+            this.TasksView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TasksView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Tasks,
+            this.Description,
+            this.TaskCompleteBy});
+            this.TasksView.Location = new System.Drawing.Point(251, 44);
+            this.TasksView.Name = "TasksView";
+            this.TasksView.Size = new System.Drawing.Size(378, 347);
+            this.TasksView.TabIndex = 2;
+            this.TasksView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TasksView_CellContentClick);
+            // 
+            // Tasks
+            // 
+            this.Tasks.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Tasks.HeaderText = "Tasks Required";
+            this.Tasks.Name = "Tasks";
+            this.Tasks.Width = 98;
+            // 
+            // Description
+            // 
+            this.Description.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Description.HeaderText = "Task Description";
+            this.Description.Name = "Description";
+            this.Description.Width = 103;
+            // 
+            // TaskCompleteBy
+            // 
+            this.TaskCompleteBy.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.TaskCompleteBy.HeaderText = "Task Completion Date";
+            this.TaskCompleteBy.Name = "TaskCompleteBy";
+            this.TaskCompleteBy.Width = 125;
             // 
             // StaffReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 373);
+            this.ClientSize = new System.Drawing.Size(684, 433);
+            this.Controls.Add(this.TasksView);
+            this.Controls.Add(this.StaffListLabel);
+            this.Controls.Add(this.StaffListBox);
             this.Name = "StaffReport";
             this.Text = "StaffReport";
             this.Load += new System.EventHandler(this.StaffReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TasksView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox StaffListBox;
+        private System.Windows.Forms.Label StaffListLabel;
+        private System.Windows.Forms.DataGridView TasksView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tasks;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TaskCompleteBy;
     }
 }
--- Assignment3_LHISGroup/Reports/StaffReport.cs
+++ Assignment3_LHISGroup/Reports/StaffReport.cs
@@ -15,53 +15,142 @@ namespace Assignment3_LHISGroup.Reports
 {
     public partial class StaffReport : Form
     {
-        Staff currentStaff;
+        
         List<Support_Classes.Task> assignedTaskList;
         DbController dbController = new DbController();
 
-        public StaffReport(Staff givenStaffMember)
+        public StaffReport()
         {
-            assignedTaskList = new List<Support_Classes.Task>();
-            currentStaff = givenStaffMember;
             InitializeComponent();
+            assignedTaskList = new List<Support_Classes.Task>();
+
+            // POPULATES DB FOR TESTING
+            ADDTODBFORTESTING();
+
+
+            populateStaffList();
 
         }
 
-        private List<Support_Classes.Task> FindTasksAssignedTo()
+        private void ADDTODBFORTESTING()
         {
-            if (currentStaff.StaffStatus == Staff.Active.inactive)
-            {
-                Console.WriteLine("Error: Staff Member is inactive");
-                return new List<Support_Classes.Task>();
-            }
-            else
-            {
-                List<Support_Classes.Task> allTaskList = new List<Support_Classes.Task>();
-                allTaskList = dbController.GetAllTasks();
-                foreach (Support_Classes.Task task in allTaskList)
-                {
-                    if (task.AssignedTo.ID == currentStaff.ID)
-                    {
-                        DateTime date = task.CompletionDate;
-                        assignedTaskList.Add(task);
-                    }
-                }
+            
+            
+            Staff testStaff1 = new Staff("Jim", "Bastiras", "email", "00000", "blah blah", Staff.Active.active);
+            Staff testStaff2 = new Staff("Bob", "Bastiras", "email", "00340", "blah blah", Staff.Active.inactive);
+            Staff testStaff3 = new Staff("Lilly", "Bastiras", "email", "10403", "blah blah", Staff.Active.inactive);
+            Staff testStaff4 = new Staff("James", "Bastiras", "email", "15094", "blah blah", Staff.Active.active);
 
+            Support_Classes.Task.Priority testPrio = Support_Classes.Task.Priority.high;
 
+            Client testClient1 = new Client("Jimmy", "Bastiras", "kalfslasf", "aflklsafl", "00000000000", "00340602", "gmail", "Daniel", "Stone");
+            Client testClient2 = new Client("Daniel", "Stone", "kalfslasf", "aflklsafl", "00000000000", "00340602", "gmail", "Jimmy", "Bastiras");
 
+            Wedding testWedding = new Wedding("Gay Rites", testClient1, testClient2, testStaff1, new DateTime(2014, 1, 18), new DateTime(2014, 1, 22));
+            Support_Classes.Task testTask = new Support_Classes.Task("testing", "does things", testPrio, new DateTime(), testStaff1, testWedding);
 
-                return assignedTaskList;
-            }
+            dbController.AddStaff(testStaff1);
+            dbController.AddStaff(testStaff2);
+            dbController.AddStaff(testStaff3);
+            dbController.AddStaff(testStaff4);
+
+            dbController.AddClient(testClient1);
+            dbController.AddClient(testClient2);
+
+            dbController.AddWedding(testWedding);
+
+           // dbController.AddTask(testTask);
+
             
         }
 
-        
+        private void populateStaffList()
+        {
+            List<Staff> staffList = new List<Staff>();
+            staffList = dbController.GetAllStaff();
+            if (staffList.Count < 1)
+            {
+                throw new Exception("No staff in Database");
+            }
+            foreach(Staff item in staffList )
+            {
+                if (item.StaffStatus == Staff.Active.active)
+                {
+                    String staffDetails = item.ID + " " + item.FirstName + " " + item.Surname;
+                    StaffListBox.Items.Add(staffDetails);
+                }
+
+            }
+
+        }
+
+        private List<Support_Classes.Task> FindTasksAssignedTo(Staff curStaff)
+        {
+
+            List<Support_Classes.Task> allTaskList = new List<Support_Classes.Task>();
+            allTaskList = dbController.GetAllTasks();
+            foreach (Support_Classes.Task task in allTaskList)
+            {
+                if (task.AssignedTo.ID == curStaff.ID)
+                {
+                    DateTime date = task.CompletionDate;
+                    assignedTaskList.Add(task);
+                }
+            }
+            return assignedTaskList;
+      
+        }
+
+
 
         private void StaffReport_Load(object sender, EventArgs e)
         {
 
         }
 
+        private void StaffListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            int curItemID = (int)(StaffListBox.SelectedItem.ToString()[0]);
+            List<Staff> allStaff = dbController.GetAllStaff();
+            List<Support_Classes.Task> assignedTasks = new List<Support_Classes.Task>();
+
+            foreach (Staff staff in allStaff)
+            {
+                if(staff.ID == curItemID)
+                {
+                    assignedTasks = FindTasksAssignedTo(staff);
+                }
+                
+            }
+            populateTasksGridView(assignedTasks);
+            
+            
+        }
+
+        private void populateTasksGridView(List<Support_Classes.Task> assignedTasks)
+        {
+            foreach(Support_Classes.Task task in assignedTasks)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+
+                row.Cells[0].Value = task.TaskName;
+                row.Cells[1].Value = task.Description;
+                row.Cells[2].Value = task.CompleteBy;
+
+                TasksView.Rows.Add(row);
+            }
+        }
+
+        private void TasksView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+      
+
+     
 
     }
 }
