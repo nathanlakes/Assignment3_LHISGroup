2ed9b9b4fb51c24fcaee0f8cdc72606c1ef528cc
Nathan Lakes <nathan.lakes@gmail.com>
2015-07-04 14:22:31 +0930

    dbcontroller findwedding(int) fix
    Id not id in field name

--- Assignment3_LHISGroup/Database/DbController.cs
+++ Assignment3_LHISGroup/Database/DbController.cs
@@ -465,7 +465,7 @@ namespace Assignment3_LHISGroup
         public Wedding FindWedding(int id)
         {
             string query = "SELECT * FROM Wedding";
-            query += "WHERE id='@id'";
+            query += "WHERE Id='@id'";
             SqlCommand myCommand = new SqlCommand(query, _db);
             myCommand.Parameters.AddWithValue("@id", id);
 
