c5c0562bc3ebd511c458d4d34e16291b2b7c01f3
Nathan Lakes <nathan.lakes@gmail.com>
2015-07-04 15:10:11 +0930

    DbController >> FindStaff(int)
    fix error

--- Assignment3_LHISGroup/Database/DbController.cs
+++ Assignment3_LHISGroup/Database/DbController.cs
@@ -733,14 +733,13 @@ namespace Assignment3_LHISGroup
             this.openDb();
 
             SqlDataReader myReader = null;
-            string query = @"SELECT * FROM Staff WHERE Id='@Id'";
+
+            string query = @"SELECT * FROM Staff WHERE Id='@id'";
             SqlCommand myCommand = new SqlCommand(query, _db);
-            myCommand.Parameters.AddWithValue("@id", id);
-            
+            myCommand.Parameters.AddWithValue("@id", id);            
             myReader = myCommand.ExecuteReader();
 
             Staff s = makeStaff(myReader);
-            s.ID = Convert.ToInt32(myReader["Id"].ToString());
 
             this.closeDb();
 
