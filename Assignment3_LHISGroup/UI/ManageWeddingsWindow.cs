e3a7ff37f5e3ac309736fe1ecd9aad630049f730
Louise Lawrence <lawlm0004@mymail.unisa.edu.au>
2015-07-04 13:14:17 +0930

    Work in progress: adding code to extract data from the WeddingDataGridView

--- Assignment3_LHISGroup/UI/ManageWeddingsWindow.cs
+++ Assignment3_LHISGroup/UI/ManageWeddingsWindow.cs
@@ -29,6 +29,32 @@ namespace Assignment3_LHISGroup.UI
             this.Refresh();
         }
 
+        private Support_Classes.Wedding ExtractSelectedRow()
+        {
+            if (WeddingsDataGridView.SelectedRows.Count > 0 && WeddingsDataGridView.SelectedRows[0].Cells[0].Value != null)
+            {
+                int id = (int) WeddingsDataGridView.SelectedRows[0].Cells[0].Value;
+
+                string title = (string) WeddingsDataGridView.SelectedRows[0].Cells[1].Value;
+                string description = (string)WeddingsDataGridView.SelectedRows[0].Cells[2].Value;
+                string client1_fk = (string)WeddingsDataGridView.SelectedRows[0].Cells[3].Value;
+                string client2_fk = (string)WeddingsDataGridView.SelectedRows[0].Cells[4].Value;
+                string startDate = (string) WeddingsDataGridView.SelectedRows[0].Cells[5].Value;
+                string eventDate = (string)WeddingsDataGridView.SelectedRows[0].Cells[6].Value;
+                string staff_fk = (string)WeddingsDataGridView.SelectedRows[0].Cells[7].Value;
+
+
+
+                return null;
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
         private void ManageWeddingsWindow_FormClosing(object sender, FormClosingEventArgs e)
         {
             this.Visible = false;
