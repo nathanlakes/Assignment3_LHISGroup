805212ff06876e8177ccf6a51aab71e85bbfe970
CaptanSivey <jimbastiras@gmail.com>
2015-07-04 16:10:19 +0930

    Test 13

--- Assignment3_LHISGroup/Testing/DBControllerTest.cs
+++ Assignment3_LHISGroup/Testing/DBControllerTest.cs
@@ -69,6 +69,21 @@ namespace Assignment3_LHISGroup.Support_Classes
         }
 
         [Test]
+        public void AddWeddingTest3()
+        {
+            Client testClient1 = new Client("Jane", "Smith", "Jane Smith", "26 Oak Avenue", "23441212", "12098776", "jan@gmail.com", "John", "John");
+            Client testClient2 = new Client("Jim", "Deer", "Jim Deer", "861 Downtown St", "0437233892", "87323236", "bj@hotmail.com", "Bobbie", "Wright");
+            Staff testStaff = new Staff("Louise", "Lawrence", "llawrence@wedplan.com", "8321254", "cant work sundays", Staff.Active.active);
+            testController.AddClient(testClient1);
+            testController.AddClient(testClient2);
+            testController.AddStaff(testStaff);
+            Wedding testWedding = new Wedding("THIS WEDDING", "descreption", testClient1, testClient2, testStaff, new DateTime(), new DateTime());
+            testController.AddWedding(testWedding);
+            Wedding returnWedding = testController.FindWedding(testWedding);
+            Assert.AreEqual(testWedding.Title, returnWedding.Title);
+        }
+
+        [Test]
         public void AddTaskTest1()
         {
             Client testClient1 = new Client("Jane", "Smith", "Jane Smith", "26 Oak Avenue", "23441212", "12098776", "jan@gmail.com", "John", "John");
@@ -146,8 +161,7 @@ namespace Assignment3_LHISGroup.Support_Classes
         public void UpdateSupplierTest()
         {
             Supplier testSupplier = new Supplier("Jim NOT Bastiras", "22 adress treet", "sasfasfa", "asdasd", "12214", 200);
-            testController.AddSupplier(testSupplier);
-            Boolean result = testController.UpdateSupplier(3, testSupplier);
+            Boolean result = testController.UpdateSupplier(1, testSupplier);
             Assert.AreEqual(true, result);
 
         }
