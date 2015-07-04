14484ab96a19c7d0245bb936b73e58c5cf50f663
CaptanSivey <jimbastiras@gmail.com>
2015-07-04 15:10:40 +0930

    Test 10.0

--- Assignment3_LHISGroup/Testing/DBControllerTest.cs
+++ Assignment3_LHISGroup/Testing/DBControllerTest.cs
@@ -34,7 +34,7 @@ namespace Assignment3_LHISGroup.Support_Classes
         {
             Supplier testSupplier = new Supplier("Jim Bastiras", "22 adress treet", "sasfasfa", "asdasd", "12214", 200);
             testController.AddSupplier(testSupplier);
-            int i = testController.FindSupplier(testSupplier.CompanyName).Count();
+            int i = testController.FindSupplier("Jim Bastiras").Count();
             Assert.AreEqual(1,i);
             
         }
@@ -51,6 +51,21 @@ namespace Assignment3_LHISGroup.Support_Classes
             testController.AddWedding(testWedding);
             List<Wedding> testList = testController.FindWedding(testWedding.Title);
             Assert.AreEqual(testWedding.Title, testList.First());
+        }
+
+        [Test]
+        public void AddWeddingTest2()
+        {
+            Client testClient1 = new Client("Jane", "Smith", "Jane Smith", "26 Oak Avenue", "23441212", "12098776", "jan@gmail.com", "John", "John");
+            Client testClient2 = new Client("Jim", "Deer", "Jim Deer", "861 Downtown St", "0437233892", "87323236", "bj@hotmail.com", "Bobbie", "Wright");
+            Staff testStaff = new Staff("Louise", "Lawrence", "llawrence@wedplan.com", "8321254", "cant work sundays", Staff.Active.active);
+            testController.AddClient(testClient1);
+            testController.AddClient(testClient2);
+            testController.AddStaff(testStaff);
+            Wedding testWedding = new Wedding("THIS WEDDING", "descreption", testClient1, testClient2, testStaff, new DateTime(), new DateTime());
+            testController.AddWedding(testWedding);
+            Wedding returnWedding = testController.FindWedding(1);
+            Assert.AreEqual(testWedding.Title, returnWedding.Title);
         }
 
         [Test]
