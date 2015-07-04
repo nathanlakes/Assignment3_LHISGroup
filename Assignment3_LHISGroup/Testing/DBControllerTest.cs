f01b8812195fbaa949bb8254b57c7377104e09e6
CaptanSivey <jimbastiras@gmail.com>
2015-07-04 14:22:31 +0930

    Test 6.0

--- Assignment3_LHISGroup/Testing/DBControllerTest.cs
+++ Assignment3_LHISGroup/Testing/DBControllerTest.cs
@@ -68,5 +68,52 @@ namespace Assignment3_LHISGroup.Support_Classes
             Assert.AreEqual(true, i);
         }
         
+        [Test]
+        public void GetAllClientsTest()
+        {
+            List<Client> testList = testController.GetAllClients();
+            int i = testList.Count();
+            Assert.AreEqual(2, i);
+        }
+
+        [Test]
+        public void GetAllSuppliersTest()
+        {
+            List<Supplier> testList = testController.GetAllSuppliers();
+            int i = testList.Count();
+            Assert.AreEqual(2, i);
+        }
+
+        [Test]
+        public void GetAllWeddingsTest()
+        {
+            List<Wedding> testList = testController.GetAllWeddings();
+            int i = testList.Count();
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void GetAllTasksTest()
+        {
+            Client testClient1 = new Client("Jane", "Smith", "Jane Smith", "26 Oak Avenue", "23441212", "12098776", "jan@gmail.com", "John", "John");
+            Client testClient2 = new Client("Jim", "Deer", "Jim Deer", "861 Downtown St", "0437233892", "87323236", "bj@hotmail.com", "Bobbie", "Wright");
+            Staff testStaff = new Staff("Louise", "Lawrence", "llawrence@wedplan.com", "8321254", "cant work sundays", Staff.Active.active);
+            testController.AddClient(testClient1);
+            testController.AddClient(testClient2);
+            testController.AddStaff(testStaff);
+            Wedding testWedding = new Wedding("THIS WEDDING", "descreption", testClient1, testClient2, testStaff, new DateTime(), new DateTime());
+            testController.AddWedding(testWedding);
+            Task testTask = new Task("Shit", "description", Task.Priority.high, new DateTime(), testStaff, testWedding);
+            Boolean i = testController.AddTask(testTask);
+            List<Task> testList = testController.GetAllTasks();
+            int i2 = testList.Count();
+            Assert.AreEqual(1, i2);
+        }
+
+        [Test]
+        public void DeleteClientTest()
+        {
+
+        }
     }
 }
