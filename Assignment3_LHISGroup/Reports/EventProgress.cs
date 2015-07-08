2e75ae0cc89aae589ad6d451b19587ad908a8f95
CaptanSivey <jimbastiras@gmail.com>
2015-07-08 11:56:36 +0930

    Started Event Progress Graph Class

--- Assignment3_LHISGroup/Reports/EventProgress.cs
+++ Assignment3_LHISGroup/Reports/EventProgress.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Assignment3_LHISGroup.Support_Classes;
 
 namespace Assignment3_LHISGroup.Reports
 {
     public partial class EventProgress : Form
     {
+        DbController dbController = new DbController();
         public EventProgress()
         {
             InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EventProgress_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GenerateGraphBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generateGraph(String weddingName)
+        {
+            List<Support_Classes.Wedding> actualWeddingList = dbController.FindWedding(weddingName);
+            Support_Classes.Wedding actualWedding = actualWeddingList.First();
+            List<Support_Classes.Task> allTasks = dbController.GetAllTasks();
+            List<Support_Classes.Task> relatedTasks = new List<Support_Classes.Task>();
+            DateTime earliestStartDate = actualWedding.StartDate;
+            DateTime latestFinishDate = actualWedding.EventDate;
+            foreach (Support_Classes.Task thing in allTasks)
+            {
+                if (thing.Wedding.Title == weddingName)
+                {
+                    relatedTasks.Add(thing);
+                }
+            }
+
+            for (DateTime date = earliestStartDate; date.Date <= latestFinishDate.Date; date = date.AddDays(1))
+            {
+
+            }
         }
     }
 }
