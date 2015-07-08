2efa1bee4b3799095002967cca1748bbb3bf3999
CaptanSivey <jimbastiras@gmail.com>
2015-07-08 14:46:45 +0930

    Final Event Progress Graph Working

--- Assignment3_LHISGroup/Reports/EventProgress.cs
+++ Assignment3_LHISGroup/Reports/EventProgress.cs
@@ -26,40 +26,53 @@ namespace Assignment3_LHISGroup.Reports
 
         private void EventProgress_Load(object sender, EventArgs e)
         {
-
+            updateWeddingsList();
         }
-
+        private void updateWeddingsList()
+        {
+            List<Support_Classes.Wedding> allWeddings = dbController.GetAllWeddings();
+            if (allWeddings.Count == 0)
+            {
+                AllWeddingslbl.Text = "No Weddings In Database!";
+            }
+            else
+            {
+                AllWeddingslbl.Text = "";
+                foreach (Support_Classes.Wedding wedding in allWeddings)
+                {
+                    AllWeddingslbl.Text += wedding.Title + "\n";
+                }
+            }
+        }
         private void GenerateGraphBtn_Click(object sender, EventArgs e)
         {
-            /**
-            Support_Classes.Client testClient1 = new Client("Jim", "Bastiras", "Daniel Stone", "1 Cherry Lane", "555-666", "555-666", "JimmyBoy@gmail.com", "Nathan", "Lakes");
-            Support_Classes.Client testClient2 = new Client("Nathan", "Lakes", "Daniel Stone", "2 Cherry Lane", "555-777", "555-777", "N.Lakes@gmail.com", "Jim", "Bastiras");
-            Support_Classes.Staff testStaff = new Staff("Daniel", "Stone", "daniel.Stone@gmail.com", "555-888", "Furiously Jealous", Support_Classes.Staff.Active.active);
-            Support_Classes.Wedding testWedding = new Wedding("MARRIAGE", "controversial", testClient1, testClient2, testStaff, new DateTime(2015, 8, 1),
-                new DateTime(2015, 8, 30),  Support_Classes.Wedding.Status.InPreparation);
-            dbController.AddClient(testClient1);
-            dbController.AddClient(testClient2);
-            dbController.AddStaff(testStaff);
-            dbController.AddWedding(testWedding);
+            SaveToFilebtn.Enabled = true;
+            EPChart.Series["ExpectedOutstanding"].Points.Clear();
+            EPChart.Series["ActualOutstanding"].Points.Clear();
 
-            Support_Classes.Task testTask1 = new Support_Classes.Task("Task 1", "Help 1", Support_Classes.Task.Priority.low, new DateTime(2015,8, 2), testStaff, testWedding);
-            Support_Classes.Task testTask2 = new Support_Classes.Task("Task 2", "Help 2", Support_Classes.Task.Priority.low, new DateTime(2015, 8, 4), testStaff, testWedding);
-            Support_Classes.Task testTask3 = new Support_Classes.Task("Task 3", "Help 3", Support_Classes.Task.Priority.low, new DateTime(2015, 8, 15), testStaff, testWedding);
-            Support_Classes.Task testTask4 = new Support_Classes.Task("Task 4", "Help 4", Support_Classes.Task.Priority.low, new DateTime(2015, 8, 17), testStaff, testWedding);
-            Support_Classes.Task testTask5 = new Support_Classes.Task("Task 5", "Help 5", Support_Classes.Task.Priority.low, new DateTime(2015, 8, 20), testStaff, testWedding);
-
-            dbController.AddTask(testTask1);
-            dbController.AddTask(testTask2);
-            dbController.AddTask(testTask3);
-            dbController.AddTask(testTask4);
-            dbController.AddTask(testTask5);
-            **/
+            if (WeddingNameTxtBx.Text == "")
+            {
+                MessageBox.Show("Must enter a string name for wedding");
+                return;
+            }
             generateGraph(WeddingNameTxtBx.Text);
-        }
+            updateWeddingsList();
 
+        }
+        private void saveGraphToFile()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = path + "\\" + WeddingNameTxtBx.Text + ".png";
+            this.EPChart.SaveImage(fileName, System.Drawing.Imaging.ImageFormat.Png);
+        }
         private void generateGraph(String weddingName)
         {
             List<Support_Classes.Wedding> actualWeddingList = dbController.FindWedding(weddingName);
+            if (actualWeddingList.Count == 0)
+            {
+                MessageBox.Show("No wedding found with this title.");
+                return;
+            }
             Support_Classes.Wedding actualWedding = actualWeddingList.First();
             List<Support_Classes.Task> allTasks = dbController.GetAllTasks();
             List<Support_Classes.Task> relatedTasks = new List<Support_Classes.Task>();
@@ -79,7 +92,7 @@ namespace Assignment3_LHISGroup.Reports
                 foreach (Support_Classes.Task thing in relatedTasks)
                 {
                     int i = DateTime.Compare(date, thing.CompleteBy);
-                    if ( i <= 0)
+                    if (i <= 0)
                     {
                         expectedTasks += 1;
                     }
@@ -93,6 +106,12 @@ namespace Assignment3_LHISGroup.Reports
                 expectedTasks = 0;
                 actualTasks = 0;
             }
+        }
+
+
+        private void SaveToFilebtn_Click_1(object sender, EventArgs e)
+        {
+            saveGraphToFile();
         }
     }
 }
